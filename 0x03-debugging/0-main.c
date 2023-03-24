d9f8d5a38d82372c16b4fc183f15e55c6b6c3a73
salahbltr <salaheddine.bellitir@gmail.com>
2023-03-24 16:38:47 -0700

    a

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function positive_or_nigative when i is zero
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
